7cc0fae2f5dba60aa748a4cf900849622a60ed43
shipra goyal <shipra.goyal@growthnatives.com>
2023-06-20 18:18:32 +0530

    adding css files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './assests/scss/style.scss'
 import { MainLayout } from './Component/MainLayout';
+import {Login} from './Component/Login';
 
 function App() {
   return (
     <>
-      <MainLayout/>
+      {/* <MainLayout/> */}
+      <Login />
     </>
   );
 }

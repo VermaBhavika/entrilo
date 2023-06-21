9e8fbe826d52c1e6d66942f2c2c780bf3561d708
shipra goyal <shipra.goyal@growthnatives.com>
2023-06-21 16:16:24 +0530

    login register design done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './assests/scss/style.scss';
 import './assests/fonts/boxicons.css'
 import { MainLayout } from './Component/MainLayout';
 import {Login} from './Component/Login';
+import { Register } from './Component/Register';
+import { ForgotPassword } from './Component/ForgotPassword';
 
 function App() {
   return (
     <>
-      <MainLayout/>
+      {/* <MainLayout/> */}
       <Login />
+      <Register />
+      <ForgotPassword />
     </>
   );
 }
--- src/Component/Register.jsx
+++ src/Component/Register.jsx
@@ -1,45 +1,41 @@
-export const Login=()=>{
+export const Register = ()=>{
     return(
         <>
-           
             <div className="container-xxl">
       <div className="authentication-wrapper authentication-basic container-p-y">
         <div className="authentication-inner">
-         
+       
           <div className="card">
             <div className="card-body">
-            
+           
               <div className="app-brand justify-content-center">
                 <a href="index.html" className="app-brand-link gap-2">
-                  <span className="app-brand-logo demo app-brand-text demo text-body fw-bolder">
-                  Entrilo
-                  </span>
-                
+                 
+                  <span className="app-brand-text demo text-body fw-bolder">entrilo</span>
                 </a>
               </div>
-             
-              <h4 className="mb-2">Welcome to Entrilo! 👋</h4>
-              <p className="mb-4">Please sign-in to your account and start the adventure</p>
+           
+              <h4 className="mb-2">Adventure starts here 🚀</h4>
+              <p className="mb-4">Make your app management easy and fun!</p>
 
               <form id="formAuthentication" className="mb-3" action="index.html" method="POST">
                 <div className="mb-3">
-                  <label for="email" className="form-label">Email or Username</label>
+                  <label for="username" className="form-label">Username</label>
                   <input
                     type="text"
                     className="form-control"
-                    id="email"
-                    name="email-username"
-                    placeholder="Enter your email or username"
+                    id="username"
+                    name="username"
+                    placeholder="Enter your username"
                     autofocus
                   />
                 </div>
+                <div className="mb-3">
+                  <label for="email" className="form-label">Email</label>
+                  <input type="text" className="form-control" id="email" name="email" placeholder="Enter your email" />
+                </div>
                 <div className="mb-3 form-password-toggle">
-                  <div className="d-flex justify-content-between">
-                    <label className="form-label" for="password">Password</label>
-                    <a href="javascript:void(0);">
-                      <small>Forgot Password?</small>
-                    </a>
-                  </div>
+                  <label className="form-label" for="password">Password</label>
                   <div className="input-group input-group-merge">
                     <input
                       type="password"
@@ -52,31 +48,31 @@ export const Login=()=>{
                     <span className="input-group-text cursor-pointer"><i className="bx bx-hide"></i></span>
                   </div>
                 </div>
+
                 <div className="mb-3">
                   <div className="form-check">
-                    <input className="form-check-input" type="checkbox" id="remember-me" />
-                    <label className="form-check-label" for="remember-me"> Remember Me </label>
+                    <input className="form-check-input" type="checkbox" id="terms-conditions" name="terms" />
+                    <label className="form-check-label" for="terms-conditions">
+                      I agree to
+                      <a href="javascript:void(0);">privacy policy & terms</a>
+                    </label>
                   </div>
                 </div>
-                <div className="mb-3">
-                  <button className="btn btn-primary d-grid w-100" type="submit">Sign in</button>
-                </div>
+                <button className="btn btn-primary d-grid w-100">Sign up</button>
               </form>
 
               <p className="text-center">
-                <span>New on our platform?</span> 
-                  <a href="javascript:void(0);">
-                  <span> Create an account</span>
+                <span>Already have an account? </span>
+                <a href="javascript:void(0);">
+                  <span>Sign in instead</span>
                 </a>
               </p>
             </div>
           </div>
-      
+        
         </div>
       </div>
     </div>
-
         </>
     );
 };
-

c237e30d27aaf787189c6281edffce37c7943aae
shipra goyal <shipra.goyal@growthnatives.com>
2023-06-28 18:35:58 +0530

    classname added

--- src/Component/reusableComponents/Forms.jsx
+++ src/Component/reusableComponents/Forms.jsx
@@ -1,8 +1,8 @@
 const Form_data = [
-  { label: 'FULL NAME', type: 'text', icon: <i className="bx bx-user"></i> },
-  { label: 'COMPANY', type: 'text', icon: <i className="bx bx-buildings"></i> },
-  { label: 'EMAIL', type: 'email', icon: <i className="bx bx-envelope"></i> },
-  { label: 'PHONE NO', type: 'text', icon: <i className="bx bx-phone"></i> },
+  { label: 'FULL NAME', type: 'text', icon: <i classNameName="bx bx-user"></i> },
+  { label: 'COMPANY', type: 'text', icon: <i classNameName="bx bx-buildings"></i> },
+  { label: 'EMAIL', type: 'email', icon: <i classNameName="bx bx-envelope"></i> },
+  { label: 'PHONE NO', type: 'text', icon: <i classNameName="bx bx-phone"></i> },
 ];
 
 export const Forms = () => {
@@ -10,27 +10,27 @@ export const Forms = () => {
     Form_data?.length > 0 && 
     <>
 
-      <div className="content-wrapper">
+      <div classNameName="content-wrapper">
 
-        <div className="container-xxl flex-grow-1 container-p-y">
-          <h4 className="fw-bold py-3 mb-4"><span className="text-muted fw-light">Forms/</span> Vertical Layouts</h4>
-          <div className="row">
-            <div className="col-xl">
-              <div className="card mb-4">
-                <div className="card-body">
+        <div classNameName="container-xxl flex-grow-1 container-p-y">
+          <h4 classNameName="fw-bold py-3 mb-4"><span classNameName="text-muted fw-light">Forms/</span> Vertical Layouts</h4>
+          <div classNameName="row">
+            <div classNameName="col-xl">
+              <div classNameName="card mb-4">
+                <div classNameName="card-body">
                   <form>
 
                     {Form_data.map((item) => (
                       <>
-                        <div className="mb-3">
-                          <label className="form-label" for="basic-icon-default-company">{item.label}</label>
-                          <div className="input-group input-group-merge">
-                            <span id="basic-icon-default-company2" className="input-group-text">
+                        <div classNameName="mb-3">
+                          <label classNameName="form-label" for="basic-icon-default-company">{item.label}</label>
+                          <div classNameName="input-group input-group-merge">
+                            <span id="basic-icon-default-company2" classNameName="input-group-text">
                               {item.icon}
                             </span>
                             <input
                               type={item.type}
-                              className="form-control"
+                              classNameName="form-control"
                               placeholder={item.label}
                               aria-label={item.label}
                             />
@@ -38,22 +38,22 @@ export const Forms = () => {
                         </div>
                       </>
                     ))}
-                    <div className="mb-3">
-                      <label class="form-label" for="basic-icon-default-message">Message</label>
-                      <div class="input-group input-group-merge">
-                        <span id="basic-icon-default-message2" class="input-group-text"
-                        ><i class="bx bx-comment"></i
+                    <div classNameName="mb-3">
+                      <label className="form-label" for="basic-icon-default-message">Message</label>
+                      <div className="input-group input-group-merge">
+                        <span id="basic-icon-default-message2" className="input-group-text"
+                        ><i className="bx bx-comment"></i
                         ></span>
                         <textarea
                           id="basic-icon-default-message"
-                          class="form-control"
+                          className="form-control"
                           placeholder="Hi, Do you have a moment to talk Joe?"
                           aria-label="Hi, Do you have a moment to talk Joe?"
                           aria-describedby="basic-icon-default-message2"
                         ></textarea>
                       </div>
                     </div>
-                    <button type="submit" className="btn btn-primary">Send</button>
+                    <button type="submit" classNameName="btn btn-primary">Send</button>
                   </form>
                 </div>
               </div>
